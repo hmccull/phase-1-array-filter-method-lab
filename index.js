056bf9163da00459cb5542f69480027e858a2738
Hannah McCullough <hmac091@gmail.com>
2021-11-23 22:49:51 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution here
+function findMatching(arrayOfDrivers, name) {
+    let newArray = [];
+
+    for (const driver of arrayOfDrivers) {
+        if (driver.toLowerCase() === name.toLowerCase()) {
+            newArray.push(driver);
+        }
+    }
+
+    return newArray;
+}
+
+function fuzzyMatch(arrayOfDrivers, beginsWith) {
+    let newArray = [];
+
+    for (const driver of arrayOfDrivers) {
+        if (driver.startsWith(beginsWith)) {
+            newArray.push(driver);
+        }
+    }
+
+    return newArray;
+}
+
+function matchName(driverObjects, name) {
+    let newArray = [];
+
+    for (const driver of driverObjects) {
+        if (driver.name === name) {
+            newArray.push(driver);
+        }
+    }
+    return newArray;
+}
