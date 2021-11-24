289497e633aecadac218a2b45e9750f707124c18
Hannah McCullough <hmac091@gmail.com>
2021-11-24 16:47:29 -0700

    commit tree 60e1ae0e199241baf7d12005dc8655faf1e15299

--- index.js
+++ index.js
@@ -1,34 +1,17 @@
-function findMatching(arrayOfDrivers, name) {
-    let newArray = [];
+const findMatching = (driverArray, driver) => {
+    return driverArray.filter(human => {
+      return human.toLowerCase() === driver.toLowerCase()
+    })
+  }
 
-    for (const driver of arrayOfDrivers) {
-        if (driver.toLowerCase() === name.toLowerCase()) {
-            newArray.push(driver);
-        }
-    }
-
-    return newArray;
+const fuzzyMatch = (driverArray, beginsWith) => {
+    return driverArray.filter(driver => {
+        return driver.startsWith(beginsWith)
+    })
 }
 
-function fuzzyMatch(arrayOfDrivers, beginsWith) {
-    let newArray = [];
-
-    for (const driver of arrayOfDrivers) {
-        if (driver.startsWith(beginsWith)) {
-            newArray.push(driver);
-        }
-    }
-
-    return newArray;
-}
-
-function matchName(driverObjects, name) {
-    let newArray = [];
-
-    for (const driver of driverObjects) {
-        if (driver.name === name) {
-            newArray.push(driver);
-        }
-    }
-    return newArray;
+const matchName = (driverArray, driver) => {
+    return driverArray.filter(driverObj => {
+        return driverObj.name === driver;
+    })
 }
